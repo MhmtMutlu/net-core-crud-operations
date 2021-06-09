651db5329fb7fe4ff86a01bc7e1688b95f7e98a8
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-09 15:23:01 +0300

    IUserService, UserManager and Message created in Business layer to realize crud operations.

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        // CRUD Operations defined with utilies (as a type) which is created in Core
+        IDataResult<List<User>> GetAll();
+        IResult Add(User user);
+        IResult Delete(User user);
+        IResult Update(User user);
+
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    // Created messages to show status of operations
+    public static class Messages
+    {
+        public static string UsersListed = "Kullanıcılar listendi!";
+        public static string Deleted = "Silme işlemi başarılı!";
+        public static string Updated = "Güncelleme işlemi başarılı!";
+        public static string Added = "Ekleme işlem başarılı!";
+    }
+}
