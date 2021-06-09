3b4a55bf25256edab9a4ca0ce663ebf4468c6284
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-09 15:44:07 +0300

    Tested UserManager's Add method in ConsoleUI layer.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -9,6 +10,18 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             UserManager userManager = new UserManager(new EfUserDal());
+
+            User user1 = new User();
+            user1.Id = 3;
+            user1.Name = "Hazal";
+            user1.Surname = "Mutlu";
+            user1.Email = "hazalmutlu@outlook.com";
+            user1.BirthDate = new DateTime(day: 17, month: 03, year: 1997);
+            user1.Phone = "05554443322";
+            user1.Photo = "hazal.jpg";
+            user1.Location = "Kayseri";
+
+            userManager.Add(user1);
 
             var result = userManager.GetAll();
 
