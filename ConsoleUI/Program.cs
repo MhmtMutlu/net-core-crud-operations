7ad7ddf662f3f89c01da120e15a7ada801edfa0d
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-09 15:29:17 +0300

    Tested UserManager's GetAll method in ConsoleUI layer.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            var result = userManager.GetAll();
+
+            if (result.Success == true)
+            {
+                foreach (var user in userManager.GetAll().Data)
+                {
+                    Console.WriteLine(user.Id + " - " + user.Name + "  " + user.Surname + " - " + user.Email);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
         }
     }
 }
