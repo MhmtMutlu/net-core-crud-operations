f4fd6f053a051625edf6dbc04129a8a89886eb6e
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-06-09 13:11:22 +0300

    IEntity.cs and User.cs created and User's properties defined.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public interface IEntity
+    {
+        // An entity interface which will be implemented by all entities such as person, employee etc.
+    }
+}
---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        // Created an user class which is implemented from IEntity to hold user's informations
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+        public string Location { get; set; }
+        public string Photo { get; set; }
+    }
+}
